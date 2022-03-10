f6565af6617d2841ea48384a6174f10b4a78433a
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2022-03-11 00:10:03 +0530

    Tuning Required

--- Code/CollectImage.py
+++ Code/CollectImage.py
@@ -3,7 +3,7 @@ import os
 import time
 import uuid
 
-ImagePath = "Code\Tensorflow\workspace\images\CollectedImages\\"
+ImagePath = "E:/GHClones/FingerSpell/Code/Tensorflow/workspace/images/CollectedImages"
 
 labels = [
     "a",
@@ -48,12 +48,22 @@ NumberOfImages = 20  # 15 for training 5 for testing
 ## ! Images Collection code
 
 for label in labels:
-    os.mkdir
-    {"Code\Tensorflow\workspace\images\CollectedImages\\" + label}
+    os.mkdir("Code\Tensorflow\workspace\images\CollectedImages\\" + label)
     cam = cv2.VideoCapture(0)
 
     print("Collecting images for {}".format(label))
-    time.sleep(5)
 
+    time.sleep(3)
     for imgNum in range(NumberOfImages):
         ret, frame = cam.read()
+        imageName = os.path.join(
+            ImagePath, label, label + "." + "{}.jpg".format(str(uuid.uuid1()))
+        )
+        cv2.imwrite(imageName, frame)
+        cv2.imshow("frame", frame)
+        time.sleep(4)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cam.release()
